3f5f090d70f2af6e76297c6a232bd6d2597ae160
gladiatorAsh <ashutosh.singh@sjsu.edu>
2016-11-17 12:08:03 -0800

    Changed css of ratings. Added cookie to prevent re-rating

--- js/rating.js
+++ js/rating.js
@@ -1,7 +1,33 @@
-$(function() {
+
+$(function () {
+    var id = $('.rate').attr('id');
+    var tempVal = getCookie("star", id);
+    if (tempVal != 0) {
+        $('.rate').rating('rate', tempVal);
+        $('.rate').attr('data-readonly', '');
+    }
+    function getCookie(cname, url) {
+        var val = $.cookie(cname);
+        console.log(cname);
+        console.log(val);
+        if (val) {
+            console.log("Reading cookie " + cname);
+            console.log("Cookie value: " + val);
+            var ca = val.split('+');
+            for (var i = 0; i < ca.length; i++) {
+
+                if (ca[i].indexOf(url) !== -1) {
+                    //echo 'true';
+                    return ca[i].split(';')[1];
+                }
+            }
+            return 0;
+        }
+        return 0;
+    }
 
     $('.rate').rating({
-        extendSymbol: function() {
+        extendSymbol: function () {
 
             $(this).tooltip({
                 container: 'body',
@@ -10,8 +36,20 @@ $(function() {
             });
 
             //$(this).on('rating.rateleave', function () {
-            $('.rate').on('change', function() {
+            $('.rate').on('change', function () {
                 //alert("hello");
+
+                var ratingVal = getCookie("star", id);
+                console.log("Rating value: " + ratingVal);
+                if (ratingVal != 0) {
+                    $('.rate').rating('rate', ratingVal);
+                    $('.rate').attr('data-readonly', '');
+                    // alert("You have rated this item already");
+                    return;
+
+                }
+
+
                 var rate = $('.rate').rating('rate');
                 $("#dialog-confirm").dialog({
                     resizable: false,
@@ -19,31 +57,36 @@ $(function() {
                     width: 400,
                     modal: true,
                     buttons: {
-                        "Confirm": function() {
+                        "Confirm": function () {
                             $(this).dialog("close");
                             $(this).tooltip('hide');
                             title = "This service has been rated";
-                            var id = $('.rate').attr('id');
+
                             console.log(id);
                             $.ajax({
                                 url: 'services/rate.php',
                                 type: 'post',
                                 data: { 'action': 'rate', 'id': id, 'rating': rate },
-                                success: function(data, status) {
+                                success: function (data, status) {
+                                    console.log(data);
                                     if (data == "ok") {
-                                        console.log(data);
+                                        //console.log(data);
                                         console.log("Readonly");
                                         $('.rate').attr('data-readonly', '');
+
+                                        var temp = id + ";" + rate + "+";
+                                        $.cookie("star", temp);
+
                                         //  $('.rate').rating(data);
                                     }
                                 },
-                                error: function(xhr, desc, err) {
+                                error: function (xhr, desc, err) {
                                     console.log(xhr);
                                     console.log("Details: " + desc + "\nError:" + err);
                                 }
                             }); // end ajax call
                         },
-                        Cancel: function() {
+                        Cancel: function () {
                             $(this).dialog("close");
                             console.log("Close");
                             $('.rate').rating('rate', '');
@@ -54,4 +97,4 @@ $(function() {
             });
         }
     });
-}());
+} ());
--- services/rate.php
+++ services/rate.php
@@ -2,32 +2,43 @@
 
 $db_host = 'ec2-54-235-76-139.compute-1.amazonaws.com';
 
+
 $db_port=5432;
+
 
 $db_username = 'jrpjshwnodylga';
 
+
 $db_password = 'uc9RYJi6HubC8h03v2VsIrsBUM';
 
+
 $db_name = 'd6s4nh527enkjs';
+
 
 
 $link= pg_connect("host=$db_host port=$db_port dbname=$db_name user=$db_username password=$db_password")
 or die('Could not connect: ' . pg_last_error());
 
 
-if($_POST['action'] == "rate") {
-    $rating=(float)$_POST["rating"];
-    $url=$_POST["id"];
 
-    $query = "UPDATE services SET total_ratings = total_ratings +$rating, no_ratings=no_ratings+1  where url='$url'";
-    
-    pg_query($query)
-    or die(pg_last_error());
-    
-    pg_close($link);	
-	//e	cho $_POST['rating'];
+if($_POST['action'] == "rate") {
+	
+	$rating=(float)$_POST["rating"];
+	
+	$url=$_POST["id"];
+	
+	
+	$query = "UPDATE services SET total_ratings = total_ratings +$rating, no_ratings=no_ratings+1  where url='$url'";
+	
+	
+	pg_query($query)
+	or die(pg_last_error());
+	
+	
+	pg_close($link);
 	
 	echo "ok";
+	
 	
 }
 
